23078b4be8991899c35b87c0d6575c4366b11e4a
eldkfnrk <johongjin01@gmail.com>
2026-04-04 23:59:30 +0900

    연결 리스트에 사용할 노드 추가

--- DataStructPractice/MyOwnLinkedList.h
+++ DataStructPractice/MyOwnLinkedList.h
@@ -1,6 +1,13 @@
 #pragma once
 
 namespace DataStruct {
+	// 2026-04-04
+	// 연결 리스트에 뭐가 있는지 노드에 뭐가 있을지 생각해보고 주석으로 저장
+	struct Node {
+		// 데이터
+		// 다음 노드 시작 주소
+	};
+
 	class OwnLinkedList
 	{
 	};
