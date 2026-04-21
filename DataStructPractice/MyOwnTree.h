8909f10c382ad6a6a603dad30cb73e3f6043a11e
eldkfnrk <johongjin01@gmail.com>
2026-04-21 13:19:29 +0900

    노드 탐색 기능 추가 삽입 연산을 왼쪽부터 채워넣기가 아닌 특정 노드 아래로 삽입되도록 변경

--- DataStructPractice/MyOwnTree.h
+++ DataStructPractice/MyOwnTree.h
@@ -30,24 +30,45 @@ namespace DataStruct {
 		}
 
 		OwnTree(T data) {
-			root = new Node<T>();
-			root->data = data;
-			root->firstNode = nullptr;
-			root->secondNode = nullptr;
+			// 이런 식으로 new를 사용함과 동시에 구조체 멤버 값 초기화가 가능하다.
+			root = new Node<T>{ data, nullptr, nullptr };
 		}
 
-		// 삽입 기능(삽입은 root부터 시작해서 아래 계층으로 내려가서 아래 계층의 왼쪽부터 삽입한다. bool 반환 타입을 가지도록 한 것은 삽입 연산이 재귀적으로 돌아야 하는데 재귀를 멈춰야 할 시점을 검사하기 위해서이다.)
-		// 디폴트 매개 변수를 통해 사용자는 데이터만 넣으면 되고 내부에서는 계속해서 다음 검사 노드를 불러와서 검사한다.
-		bool InsertNode(T data, Node<T>* checkNode = root) {
-			if (!checkNode) {  // 검사하는 노드가 비어있다면 해당 노드에 값을 추가
-				checkNode = new Node<T>();
-				checkNode->data = data;
-				checkNode->firstNode = nullptr;
-				checkNode->secondNode = nullptr;
-				return true;
-			}
+		// 그 동안 순차적 자료구조를 알아보면서 나도 모르게 이것 또한 정해진 순서대로 값을 삽입해야 한다는 착각을 해버렸다.
+		// 그러나 트리는 계층 구조이기 때문에 특정 노드가 특정 노드 밑으로 삽입되어야 한다는 것을 알게 되었다.
+		// 그렇기 때문에 특정 노드를 찾고 그 아래에 들어갈 공간이 있는지 확인하고 없다면 삽입 불가를 있다면 삽입을 진행하는 구조로 가야 한다.
 
-			// 재귀 함수를 어떻게 설계할지 더 알아보기(04-18)
+		// 탐색 기능
+		Node<T>* FindNode(const T& data, Node<T>* checkNode = root) {
+			if (!checkNode) 
+				return nullptr;
+
+			// 현재 탐색 중인 노드의 데이터와 탐색하고자 하는 데이터를 비교하여 맞다면 해당 노드를 반환하고 아니라면 하위 노드를 탐색
+			// 만약 하위 노드가 없다면 오른쪽 노드를 탐색하는 식으로 진행
+			if (checkNode->data == data)
+				return checkNode;
+
+			Node<T>* firstFindNode = FindNode(data, checkNode->firstNode);  // 왼쪽 노드를 쭉 순회해서 해당 데이터를 가진 노드가 있는지 확인
+
+			if (firstFindNode)
+				return firstFindNode;
+
+			return FindNode(data, checkNode->secondNode);  // 왼쪽 노드들을 쭉 탐색하다가 발견하지 못하였다면 오른쪽 노드들도 탐색
+
+			// 위의 구현된 동작들을 보면 쭉 해당 노드의 데이터를 비교 후 아니면 쭉 왼쪽으로 탐색하다가 왼쪽 노드가 비었다면 오른쪽 노드를 탐색해보는 식이다. 조금 더 이해가 쉽게 트리의 구조와 탐색 순서를 시각적으로 보여주면 아래와 같다.
+			//      1
+			//   2     3
+			// 4  5  6   7
+			// 탐색 순서 : 1 - 2 - 4 - 5 - 3 - 6 - 7
+		}
+
+		// 삽입 기능
+		// 매개변수에 const 참조형을 사용한 이유는 혹시 모를 값 변형을 막고 배열 등의 데이터가 매개변수로 들어올 수 있도록 하기 위해서이다.
+		// 앞의 매개변수는 삽입할 위치의 상위 노드 데이터, 뒤의 매개변수는 삽입할 데이터를 나타낸다.
+		bool InsertNode(const T& topData, const T& data) {
+			Node<T>* parentNode = FindNode(topData);
+
+			// 삽입할 위치는 상위 노드의 아래이기 때문에 상위 노드를 먼저 찾고 그 아래에 삽입 가능한지를 찾는 연산이 진행되어야 한다.(04-21)
 		}
 
 
